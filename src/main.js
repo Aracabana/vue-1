8c8d1305fa38ba9a56707202bedaf5ec306065df
YKadzilnikova <ykadzilnikova@itsupportme.com>
2020-10-13 14:03:07 +0300

    updates

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import router from './router';
 import firebase from 'firebase';
 
 Vue.config.productionTip = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyD11AE4T6bMF2p_HRF1WLjpDusr3Z8P1Do",
+  authDomain: "training-53585.firebaseapp.com",
+  databaseURL: "https://training-53585.firebaseio.com",
+  projectId: "training-53585",
+  storageBucket: "training-53585.appspot.com",
+  messagingSenderId: "649189057763",
+  appId: "1:649189057763:web:422e60142ab5110d7991d6"
+};
 
 new Vue({
   router,
   render: h => h(App),
   created() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyD11AE4T6bMF2p_HRF1WLjpDusr3Z8P1Do",
-      authDomain: "training-53585.firebaseapp.com",
-      databaseURL: "https://training-53585.firebaseio.com",
-      projectId: "training-53585",
-      storageBucket: "training-53585.appspot.com",
-      messagingSenderId: "649189057763",
-      appId: "1:649189057763:web:422e60142ab5110d7991d6"
-    };
     firebase.initializeApp(firebaseConfig);
   }
 }).$mount('#app');
